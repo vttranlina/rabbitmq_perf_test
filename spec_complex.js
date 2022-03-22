aa028ab9b89e46c6222a74ccacfd466459e9f406
Tran Van Tung <tungtv202@gmail.com>
2022-03-22 03:31:33 +0000

    init

---
+++ spec_complex.js
@@ -0,0 +1,9 @@
+[{'name': 'performance-test', 'type': 'varying',
+'uri': 'amqp://james:eeN7Auquaeng@localhost:5677',
+'params': [{'time-limit': 60,
+    'producer-count': 2,
+    'consumer-count': 4, 
+    'exchange-name': 'performanceTestExchangeName2'
+ }],
+'variables': [{'name':   'min-msg-size', 'values': [100, 10]}]
+}]
