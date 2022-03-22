b610d70738838df00475667101406dc9c4888152
Tran Van Tung <tungtv202@gmail.com>
2022-03-22 03:46:59 +0000

    2

--- spec_complex.js
+++ spec_complex.js
@@ -1,5 +1,5 @@
 [{'name': 'performance-test', 'type': 'varying',
-'uri': 'amqp://james:eeN7Auquaeng@localhost:5677',
+'uri': 'amqp://james:eeN7Auquaeng@10.2.3.6:5672',
 'params': [{'time-limit': 60,
     'producer-count': 2,
     'consumer-count': 4, 
